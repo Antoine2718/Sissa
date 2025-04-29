09e6f00ac0b9e1a4b7164db11c9265a751e949cf
E-2Pi <172035350+E-2Pi@users.noreply.github.com.>
2025-04-29 15:57:57 +0200

    Ajout de quelques items et suppression d'autres

--- ScriptSQL/createDB.sql
+++ ScriptSQL/createDB.sql
@@ -201,24 +201,27 @@ INSERT INTO utilisateur (points,identifiant,mdp,type,idRang) VALUES
 (253,"BraveLava","$2y$10$qgsA4FPow6oLrM5OX9dfo.Wzk0DxnFx8tAd2Rnl3NZX/QNo0o3dMK","user",2), /*Mot de passe: test */
 (1125,"Epiccc","$2y$10$qgsA4FPow6oLrM5OX9dfo.Wzk0DxnFx8tAd2Rnl3NZX/QNo0o3dMK","user",5) /*Mot de passe: test */
 ;
+ALTER TABLE achete DROP PRIMARY KEY;
+-- Ce changement est fait afin de permettre au même utilisateur d'acheter le même article plusieurs fois à des dates différentes.
+-- On ajoute une clé primaire composite sur idUtilisateur, idArticle et date_achat
+ALTER TABLE achete ADD PRIMARY KEY(idUtilisateur, idArticle, date_achat);
+
 ALTER TABLE Article
 -- Ajouter une colonne 'categorie' à la table 'Article'
 ADD categorie VARCHAR(30) NOT NULL DEFAULT 'autre';
 
 -- Insérer des articles dans la catégorie 'vêtements'
 INSERT INTO `article` (`idArticle`, `prix`, `nom`, `stock`, `description`, `lien_image`, `categorie`) VALUES
-(1, 19.99, 'T-shirt \"Morpion Master\"', 50, 'T-shirt noir avec logo morpion en style pixel art.', 'tshirt-morpion.jpg', 'vêtements'),
+(1, 19.99, 'T-shirt \"Morpion Masterr\"', 50, 'T-shirt gris inscription MASTERR.\r\nDouble R, pour un double résultat !\r\n100% coton pour un confort tout au long de la journée', '..\\images\\Shop-images\\tshirt-morpion.png', 'vêtements'),
 (2, 34.90, 'Hoodie \"Think Like a Cross\"', 30, 'Hoodie bleu marine, 80% coton, 20% polyester pour une finition éclatante et un confort élevé tout au long de la journée.', '..\\images\\Shop-images\\sweat-think-crss.png', 'vêtements'),
-(3, 16.50, 'Casquette \"O Player\"', 39, 'Casquette noire brodée avec un grand O blanc.', 'casquette-o.jpg', 'vêtements'),
 (4, 28.00, 'T-shirt \"X Marks the Spot\"', 58, 'T-shirt blanc 100% coton pour un confort tout au long de la journée.', '..\\images\\Shop-images\\x-marks.png', 'vêtements'),
-(5, 29.90, 'Hoodie \"O Strategy\"', 25, 'Hoodie bleu marine, 80% coton, 20% polyester pour une finition éclatante et un confort élevé tout au long de la journée.', '..\\images\\Shop-images\\sweat-o-strat.png', 'vêtements'),
-(6, 18.50, 'Casquette \"Xtreme\"', 35, 'Casquette rouge avec un X brodé en noir.', 'casquette-xtreme.jpg', 'vêtements');
+(5, 29.90, 'Hoodie \"O Strategy\"', 25, 'Hoodie bleu marine, 80% coton, 20% polyester pour une finition éclatante et un confort élevé tout au long de la journée.', '..\\images\\Shop-images\\sweat-o-strat.png', 'vêtements');
 
 -- Insérer des articles dans la catégorie 'accessoires'
 INSERT INTO `article` (`idArticle`, `prix`, `nom`, `stock`, `description`, `lien_image`, `categorie`) VALUES
 (7, 11.99, 'Mug \"Game On\"', 100, 'Mug 325ml - idéal pour un latte - en céramique blanche et brillante avec grille de morpion imprimée.', '..\\images\\Shop-images\\game-on-tasse.png', 'accessoires'),
 (8, 9.99, 'Calendrier stratégique 2025', 70, 'Un mois = une citation + une grille de morpion.\r\nChaque calendrier comporte une reliure à anneaux, 26 pages et une finition qualité satinée. Parfait pour être accroché à votre mur.', '..\\images\\Shop-images\\calendrier-strat.png', 'accessoires'),
-(9, 2.99, 'Stylo \"Xtra Point\"', 120, 'Stylo bille avec embout en forme de X.', 'stylo-x.jpg', 'accessoires'),
+(9, 22.49, 'Gourde \"Masterr\"', 120, 'Gourde Masterr, double R pour un DOUBLE RESULTAT !\r\nChaque gourde est fabriquée en acier inoxydable alimentaire et est livrée avec un bouchon à vis.\r\nGarde vos boissons fraîches pendant 24h et chaudes pendant 12h ( faut bien hydrater votre cerveau, ce génial instrument de bataille !).', '..\\images\\Shop-images\\gourde-masterr.png', 'accessoires'),
 (10, 14.99, 'Tapis de souris \"Morpion\"', 80, 'Tapis de souris avec une grille de morpion imprimée.', 'tapis-souris-morpion.jpg', 'accessoires'),
 (11, 7.99, 'Porte-clés \"X et O\"', 150, 'Porte-clés en métal avec X et O entrelacés.', 'porte-cles-xo.jpg', 'accessoires'),
 (12, 5.50, 'Carnet \"Stratégies gagnantes\"', 90, 'Carnet de notes avec couverture morpion.', 'carnet-strategies.jpg', 'accessoires'),
@@ -254,6 +257,7 @@ INSERT INTO `article` (`idArticle`, `prix`, `nom`, `stock`, `description`, `lien
 (33, 22.50, 'Jeu \"Morpion électronique\"', 25, 'Version électronique du morpion avec sons et lumières.', 'jeu-electronique.jpg', 'jeux'),
 (34, 17.99, 'Jeu \"Mini Morpion\"', 40, 'Version portable du morpion pour jouer partout.', 'jeu-mini.jpg', 'jeux'),
 (35, 26.99, 'Jeu \"Morpion XXL\"', 15, 'Version géante du morpion pour jouer en extérieur.', 'jeu-xxl.jpg', 'jeux');
+
 -- Insertion de données de test pour les promotions
 
 -- Ajouter quelques promotions
