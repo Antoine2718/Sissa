e42c3a87683260cef2b5ff4fbbfc45d0a52d4c3a
KasterborousConstellation <paul.andrieu28@gmail.com>
2025-04-29 14:01:26 +0200

    classement

--- Projet/common/leaderboard.php
+++ Projet/common/leaderboard.php
@@ -10,20 +10,21 @@
         //Affiche les 10 meilleurs joueurs
         $db = connect();
         try{
-            $stmt = $db->prepare("SELECT u.identifiant as name,u.points as pts,r.couleur_rang as couleur,r.nomRang as rang FROM utilisateur u inner join rang r on r.idRang = u.idRang order by u.points DESC limit 10");
+            $stmt = $db->prepare("SELECT u.identifiant as name,u.points as pts,r.couleur_rang as couleur,r.nomRang as rang FROM utilisateur u inner join rang r on r.idRang = u.idRang order by u.points DESC limit 30");
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
             $i=1;
+            echo "<table>";
+            echo "<thead><tr class=\"leaderboard-element\"><th>Position</th><th>Nom</th><th>Rang</th><th>Points</th></tr></thead>";
             foreach($result as $user){
                 $name = $user['name'];
                 $pts = $user['pts'];
                 $color = $user['couleur'];
                 $rang = $user['rang'];
-                echo "<div class=\"leaderboard-element\">";
-                echo "<p>$name</p> <p style = \"color:$color;\">$rang</p> <p class =\"leaderboard-pts\">$pts</p>";
-                echo "</div>";
+                echo "<tr class=\"leaderboard-element\"><td>$i</td><td>$name</td> <td style = \"color:$color;\">$rang</td> <td class =\"leaderboard-pts\">$pts</td></tr>";
                 $i=$i+1;
             }
+            echo "</table>";
         }catch(PDOException $e){
             header("Location: ../pages/error_page.php");
             exit();
--- Projet/pages/classement.php
+++ Projet/pages/classement.php
@@ -16,10 +16,45 @@
         require_once("../common/db.php");
         include("../common/nav.php")
     ?>
-
+    
     <!-- Contenu principal -->
     <div class="content">
+        <div class="leaderboard-global-wrapper">
+
         
+        <div class="leaderboard">
+        <div class="leaderboard-head">
+            <h1 >Classement Général</h1>
+            <hr>
+        </div>
+        <?php 
+        //Affiche les 10 meilleurs joueurs
+        $db = connect();
+        try{
+            $stmt = $db->prepare("SELECT u.identifiant as name,u.points as pts,r.couleur_rang as couleur,r.nomRang as rang FROM utilisateur u inner join rang r on r.idRang = u.idRang order by u.points DESC limit 15");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $i=1;
+            echo "<table>";
+            echo "<thead><tr class=\"leaderboard-element\"><th>Position</th><th>Nom</th><th>Rang</th><th>Points</th></tr></thead>";
+            foreach($result as $user){
+                $name = $user['name'];
+                $pts = $user['pts'];
+                $color = $user['couleur'];
+                $rang = $user['rang'];
+                
+                echo "<tr class=\"leaderboard-element\"><td>$i</td><td>$name</td> <td style = \"color:$color;\">$rang</td> <td class =\"leaderboard-pts\">$pts</td></tr>";
+                
+                $i=$i+1;
+            }
+            echo "</table>";
+        }catch(PDOException $e){
+            header("Location: ../pages/error_page.php");
+            exit();
+        }
+        ?>
+        </div>
+        </div>
     </div>
     
     <?php
